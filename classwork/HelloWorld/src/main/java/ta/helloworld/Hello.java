25b8663cecb1f68a602e16fed6e20932f3e0e5ad
Tadeyoyin <tobozytozox@gmail.com>
2022-06-29 02:32:01 -0400

    Hello World in NetBeans

---
+++ classwork/HelloWorld/src/main/java/ta/helloworld/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * @author: Tolu
+ * email: tolulope.adeyoyin@yahoo.co.uk
+ * date: 29th June, 2022
+ * purpose: My first Hello World program in an IDE
+ */
+package ta.helloworld;
+
+/**
+ *
+ * 
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Tolu!");
+    }
+}
